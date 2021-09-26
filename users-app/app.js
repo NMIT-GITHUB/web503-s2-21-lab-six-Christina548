91efc16b1c6f5ba9efb0ec084b91cb99de6de504
Christina <christina-hardy@live.nmit.ac.nz>
2021-09-27 11:52:03 +1300

    firebase code added to app.js

---
+++ users-app/app.js
@@ -0,0 +1,16 @@
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAYrttd-CoF8gQOevy3vPIJYWwHTNTNhWI",
+    authDomain: "web503-lab6.firebaseapp.com",
+    projectId: "web503-lab6",
+    storageBucket: "web503-lab6.appspot.com",
+    messagingSenderId: "783081255686",
+    appId: "1:783081255686:web:8ff86d193ff6857c242487"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
